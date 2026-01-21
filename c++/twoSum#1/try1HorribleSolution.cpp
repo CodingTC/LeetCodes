25c5e657ef18f939d7961ff74aead1c05290baec
TDC <trevordcooley@gmail.com>
2026-01-21 18:04:24 -0500

    Add twoSum#1 c++ solution

---
+++ c++/twoSum#1/try1HorribleSolution.cpp
@@ -0,0 +1,47 @@
+// This is really not optimal and is a bad solution
+
+#include <iostream>
+#include <vector>
+
+class Solution 
+{
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        // to check if the indices of the two nums that add up to target are found
+        bool found = false;
+        // array for the indices to return
+        vector<int> arr;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            for(int j = i + 1; j < nums.size(); j++)
+            {
+                if(nums[i] + nums[j] == target)
+                {
+                    arr.push_back(i);
+                    arr.push_back(j);
+                    found = true;
+                    break;
+                }
+            }
+            if(found) break;
+        }
+
+        showResults(arr);
+        return arr;
+    }
+
+    void showResults(vector<int> indices)
+    {
+        cout << '[';
+        for(int i = 0; i < indices.size(); i++)
+        {
+            cout << indices[i];
+            if(i < indices.size() - 1)
+            {
+                cout << ',';
+            }
+        }
+        cout << ']';
+    }
+};
