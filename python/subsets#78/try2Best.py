1d6756becd21f5b7f591bec3a0ff75656b10c69d
TDC <trevordcooley@gmail.com>
2026-02-18 05:01:31 -0500

    Add subsets#78 python solution

---
+++ python/subsets#78/try2Best.py
@@ -0,0 +1,22 @@
+# Optimal solution
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        cur = []
+        def dfs(i):
+            if i >= len(nums):
+                res.append(cur.copy())
+                return
+            
+            cur.append(nums[i])
+            dfs(i + 1)
+            cur.pop()
+            dfs(i + 1)
+
+
+        dfs(0)
+        return res
+
+# Time: O(2^N)
+# Space: O(2^N)
